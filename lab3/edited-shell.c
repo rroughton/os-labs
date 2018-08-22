50aa7edffa594d68f2e51240f674a065241630d5
Becca <roughton.rebecca@gmail.com>
2018-08-21 23:48:28 -0400

    add name tag

--- lab3/edited-shell.c
+++ lab3/edited-shell.c
@@ -303,7 +303,7 @@ void signal_done_background()
 		printf("\npassed third sprintf\n");
 		printf(done_str);
         sprintf(done_str, "[%d]\tDone\t%d\n", background_list[i].number, background_list[i].pid); 
-		printf("\npassed sprintf");
+		printf("\npassed sprintf\n");
 		printf(done_str);
 		fflush(stdout);
 		strcpy(done_strs[num_done_strs], done_str);
